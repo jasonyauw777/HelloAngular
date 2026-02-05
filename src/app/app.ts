383b385545e65ed1912e6d865be004a5298d10db
Jason Surya <jasonsurya0@gmail.com>
2026-02-05 09:16:22 +0700

    create Hello Angular Tutorial App

---
+++ src/app/app.ts
@@ -0,0 +1,24 @@
+import {Component} from '@angular/core';
+import { Home } from './home/home';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterLink, RouterOutlet],
+  template: ` 
+    <main>
+      <a [routerLink]="['/']">
+        <header>
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet />
+      </section>
+    </main>
+     `,
+  styleUrls: ['./app.css'],
+})
+export class App {
+  title = 'homes';
+}
